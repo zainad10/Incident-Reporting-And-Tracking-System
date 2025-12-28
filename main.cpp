05348abf93485248b419517039ec9de757f3d075
zdar <zainadar696@gmail.com>
2025-12-28 22:18:56 +0500

    feat: added listNode , linkedListStack

--- main.cpp
+++ main.cpp
@@ -10,3 +10,69 @@
 #include <unordered_map>
 #include <unordered_set>
 using namespace std;
+template<typename T>
+class ListNode{
+public:
+    T data;
+    ListNode* next;
+    
+    ListNode(T value) {
+        data = value;      
+        next = NULL;  
+    }
+};
+
+template<typename T>
+class LinkedListStack{
+    ListNode<T>* top;
+    int size;
+
+public:
+    LinkedListStack(){
+        top = NULL;
+        size = 0;
+    }
+
+
+    ~LinkedListStack(){
+        while (!isEmpty()){
+            pop();
+        }
+    }
+
+    void push(T item){
+        ListNode<T>* newNode = new ListNode<T>(item);
+        newNode->next = top;
+        top = newNode;
+        size++;
+    }
+
+    T pop(){
+        if (isEmpty()) {
+            return T();
+        }
+        
+        ListNode<T>* temp = top;
+        T item = top->data;
+        top = top->next;
+        delete temp;
+        size--;
+        
+        return item;
+    }
+
+    bool isEmpty() {
+        return top == NULL;
+    }
+
+    T peek() {
+        if (isEmpty()) {
+            return T();
+        }
+        return top->data;
+    }
+
+    int getSize() {
+        return size;
+    }
+};
